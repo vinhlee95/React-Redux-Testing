d650d1c6b0883375e9be934a402826c49a7b0163
Vinhlee95 <lethanhvinh95@gmail.com>
2018-02-19 08:39:05 +0200

    Added new test & CommentBox in App

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import CommentBox from './CommentBox';
 
 export default class App extends Component {
   render() {
     return (
-      <div>Hello again</div>
+      <div>
+        <div>Hello again</div>
+        <CommentBox />
+      </div>
     );
   }
 }
--- test/components/app_test.js
+++ test/components/app_test.js
@@ -4,16 +4,13 @@ import App from '../../src/components/app';
 
 // Group together similar tests
 describe('App', () => {
+  let component;
+  beforeEach(() => {
+    component = renderComponent(App);
+  });
 
-  // Test a single attribute of a target
-  it('shows the correct text', () => {
-
-    // Create an instance of App
-    const component = renderComponent(App);
-
-    // To make an assertion about a target 
-    expect(component).to.contain('Hello again');
-
+  it('has the comment box', () => {
+    expect(component.find('.comment-box')).to.exist;
   });
 
 });
